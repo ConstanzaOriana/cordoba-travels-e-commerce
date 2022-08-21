fff87e3e0a1da4c377e4eacdfb5d46da158c031b
Constanza Oriana <cotyoriana95@gmail.com>
2022-08-21 19:46:15 -0300

    Cart

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,10 +13,13 @@ const Header = () => {
                         <a href ="#" className={estilos.links}>Gallery</a>
                     </li>
                     <li className={estilos.lista}>
-                        <a href ="#" className={estilos.links}>About Cordoba</a>
+                        <a href ="#" className={estilos.links}>About Córdoba</a>
                     </li>
                     <li className={estilos.lista}>
                         <a href ="#" className={estilos.links}>Contact</a>
+                    </li>
+                    <li className={estilos.lista}>
+                        <a href ="#" className={estilos.links}><img className={estilos.carrito} src={require('./cart.png')}/></a>
                     </li>
                 </ul>
             </nav>
