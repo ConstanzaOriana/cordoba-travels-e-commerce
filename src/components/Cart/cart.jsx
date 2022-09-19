740033edccefbe6784c2db7358d313104656c79b
Constanza Oriana <cotyoriana95@gmail.com>
2022-09-19 10:40:22 -0300

    Empty_Cart

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
-// import Form from '../Form/form';
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
+import estilos from './cart.module.css'
+//import Form from '../Form/Form';
 
 const Cart = () => {
-    const {clearCart} = useContext(CartContext)
+    const { cart, clearCart, deleteOne, totalPrice } = useContext(CartContext);
+
+    const total = totalPrice();
+
+    if (cart.length === 0) {
+        return <h1 className={estilos.title}>Start your adventure today!</h1>;
+    }; 
+
     return (
-        <div>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+            }}
+        >
             {/* <Form /> */}
+            {cart.map((prod) => (
+                <div
+                    key={prod.id}
+                    style={{
+                        border: '1px solid black',
+                        borderRadius: '8px',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        padding: '8px',
+                        margin: '8px',
+                    }}
+                >
+                    <h3>{prod.title}</h3>
+                    <h3>Quantity: {prod.cantidad}</h3>
+                    <h3>Price${prod.price}.-</h3>
+                    <button onClick={() => deleteOne(prod.id)}>Delete</button>
+                </div>
+            ))}
             <button onClick={clearCart}>Clear Cart</button>
+            <h4>Total: ${total}</h4>
         </div>
     );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,21 +5,72 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
-    
+
     const addToCart = (item, cantidad) => {
-        if (false) {
+        if (isInCart(item.id)) {
+            totalQuantitySingleProduct(item, cantidad);
         } else {
             setCart([...cart, { ...item, cantidad }]);
         }
     };
 
+    const isInCart = (id) => {
+        return cart.some((producto) => producto.id === id);
+    };
+
+    const totalQuantitySingleProduct = (item, cantidad) => {
+        const updateProducts = cart.map((prod) => {
+            if (prod.id === item.id) {
+                const productUpdated = {
+                    ...prod,
+                    cantidad: cantidad,
+                };
+
+                return productUpdated;
+            } else {
+                return prod;
+            }
+        });
+        setCart(updateProducts);
+    };
+
+    const totalQuantity = () => {};
+
+    const totalPrice = () => {
+        let acumulador = 0;
+        cart.forEach((prod) => {
+            acumulador += prod.cantidad * prod.price;
+        });
+        return acumulador;
+    };
+
+    
+    const deleteOne = (id) => {
+        const filteredProducts = cart.filter((prod) => prod.id !== id);
+        setCart(filteredProducts);
+    };
 
     const clearCart = () => {
         setCart([]);
     };
 
+    const getProductQuantity = (id) => {
+        const product = cart.find((prod) => prod.id === id);
+        return product?.cantidad;
+    };
+
     return (
-        <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+        <CartContext.Provider
+            value={{
+                cart,
+                addToCart,
+                clearCart,
+                deleteOne,
+                totalPrice,
+                totalQuantity,
+                getProductQuantity,
+            }}
+        >
             {children}
         </CartContext.Provider>
     );
